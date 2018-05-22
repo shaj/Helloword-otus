14a4e0d14b164512107c32bf51b382ee787993ee
Andrey Shaj <kolobrod2002@yandex.ru>
2018-05-22 17:04:36 +0300

    tests

--- test_main.cpp
+++ test_main.cpp
@@ -21,9 +21,9 @@ BOOST_AUTO_TEST_SUITE(test_suite_main)
 /// Набор тестов для проверки генерации версии приложения
 BOOST_AUTO_TEST_CASE(test_version_valid)
 {
-	BOOST_CHECK( version_major() == 0 );
-	BOOST_CHECK( version_minor() == 0 );
-	BOOST_CHECK( version_patch() > 0 );
+	BOOST_CHECK( version_major() >= 0 );
+	BOOST_CHECK( version_minor() >= 0 );
+	BOOST_CHECK( version_patch() >= 0 );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
